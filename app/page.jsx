f0ac69a49ea38a7f67c10361820c56e938186c05
rabin2403 <105305328+rabin2403@users.noreply.github.com>
2024-06-28 11:22:10 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,43 @@
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+// import Swiper JS
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import Image from "next/image";
+
+
+export default function Home() {
+  
+  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+    // Receiving api data
+    // update usestate
+  }, []);
+
+
+  
+
+  return (
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>
+        <Image src={"/next.svg"} width={200} height={200} />
+        
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
